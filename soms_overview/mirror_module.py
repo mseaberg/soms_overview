b6db6e086171fca6500ff8d582f36dce2997d1a4
Matthew H. Seaberg <seaberg@slac.stanford.edu>
2026-03-10 14:01:57 -0700

    renamed files to soms_overview

---
+++ soms_overview/mirror_module.py
@@ -0,0 +1,234 @@
+from PyQt5.QtCore import Qt
+from PyQt5.uic import loadUiType
+import numpy as np
+import json
+import os
+from ophyd import EpicsSignal, EpicsSignalRO
+from epics import PV
+from pydm.widgets.channel import PyDMChannel
+from pydm.data_plugins import PyDMPlugin
+import subprocess
+import threading
+import time
+#from pcdsdevices.mirror import TwinCATMirrorStripeTuple
+
+local_path = os.path.dirname(os.path.abspath(__file__))
+
+class HOMS_state:
+    def __init__(self, mirror_name, status=None):
+
+        self.name = mirror_name
+        with open(local_path+'/mirror_info.dat') as json_file:
+            info = json.load(json_file)
+       
+        mirror_info = info[self.name]
+        #self.y_pos = mirror_info['Y']
+        #self.in_pos = mirror_info['IN']
+        #self.out_pos = mirror_info['OUT']
+        self.prefix = mirror_info['prefix']
+        self.is_inout = mirror_info['inout']
+        self.is_in = False
+        self.is_out = False
+        self.destination = None
+       
+        self.current_range = None
+        
+        self.nominal_pitch = None
+        
+        if self.name=='MR1L4':
+            self.mec_ranges = mirror_info['MEC_Ranges']
+            self.mfx_ranges = mirror_info['MFX_Ranges']
+        else:
+            self.ranges = mirror_info['Ranges']
+
+        # connect to pv's
+
+        self.pitch_rbvs = {}
+
+        if self.name == 'MR1L4':
+            self.pitch_rbvs['MFX_Coating1'] = EpicsSignalRO(read_pv='{}:PITCH:MFX:Coating1'.format(self.name))
+            self.pitch_rbvs['MFX_Coating2'] = EpicsSignalRO(read_pv='{}:PITCH:MFX:Coating2'.format(self.name))
+            self.pitch_rbvs['MEC_Coating1'] = EpicsSignalRO(read_pv='{}:PITCH:MEC:Coating1'.format(self.name))
+            self.pitch_rbvs['MEC_Coating2'] = EpicsSignalRO(read_pv='{}:PITCH:MEC:Coating2'.format(self.name))
+        else:
+            self.pitch_rbvs['Coating1'] = EpicsSignalRO(read_pv='{}:PITCH:Coating1'.format(self.name))
+            self.pitch_rbvs['Coating2'] = EpicsSignalRO(read_pv='{}:PITCH:Coating2'.format(self.name))
+
+        self.x_state_rbv = EpicsSignalRO(read_pv=self.prefix+'MMS:XUP:STATE:GET_RBV')
+        self.coating_rbv = EpicsSignal(read_pv=self.prefix+'COATING:STATE:GET_RBV')
+        self.pitch_rbv = EpicsSignalRO(read_pv=self.prefix+'MMS:PITCH.RBV')
+        self.x_state = EpicsSignal(self.prefix+'MMS:XUP:STATE:SET')
+        #self.coating_motor = EpicsSignal(self.prefix+'COATING:STATE:SET')
+        self.coating_motor = PV(self.prefix+'COATING:STATE:SET')
+        self.pitch_motor = EpicsSignal(self.prefix+'MMS:PITCH')
+
+        self.coating_rbv.subscribe(self.check_coating)
+   
+        self.x_state_rbv.subscribe(self.check_inout)
+        self.pitch_rbv.subscribe(self.check_dest)
+        self.coating_moving = EpicsSignalRO(read_pv=self.prefix+'MMS:YUP.MOVN')
+        self.pitch_moving = EpicsSignalRO(read_pv=self.prefix+'MMS:PITCH.MOVN')
+
+        moving_names = ['MMS:PITCH.MOVN','MMS:XUP.MOVN','MMS:YUP.MOVN','MMS:PITCH.DMOV',
+                'MMS:XUP.DMOV','MMS:YUP.DMOV']
+
+        error_names = ['MMS:XUP:PLC:nErrorId_RBV','MMS:YUP:PLC:nErrorId_RBV','MMS:PITCH:PLC:nErrorId_RBV',
+                'MMS:XDWN:PLC:nErrorId_RBV','MMS:YDWN:PLC:nErrorId_RBV']
+
+        status_names = ['Ready','Moving','Error']
+        colors = [Qt.green,Qt.yellow,Qt.red]
+        self.status = status
+        self.status.connect(self.prefix,self.pitch_rbv, moving_names,error_names,status_names,colors)
+
+        error_names = ['MMS:XUP:PLC:nErrorId_RBV','MMS:YUP:PLC:nErrorId_RBV','MMS:PITCH:PLC:nErrorId_RBV',
+                'MMS:XDWN:PLC:nErrorId_RBV','MMS:YDWN:PLC:nErrorId_RBV']
+
+        self.check_coating(value=self.coating_rbv.get())
+
+    def update_mirror(self):
+        with open(local_path+'/mirror_info.dat') as json_file:
+            info = json.load(json_file)
+     
+        mirror_info = info[self.name]
+
+        if self.name=='MR1L4':
+            self.mec_ranges = mirror_info['MEC_Ranges']
+            self.mec_pitches = mirror_info['MEC_Pitch']
+            self.mfx_ranges = mirror_info['MFX_Ranges']
+            self.mfx_pitches = mirror_info['MFX_Pitch']
+        else:
+            self.ranges = mirror_info['Ranges']
+            self.pitches = mirror_info['Pitch']
+        self.y_pos = mirror_info['Y']
+        self.coatings = mirror_info['Coatings']
+        self.in_pos = mirror_info['IN']
+        self.out_pos = mirror_info['OUT']
+        self.prefix = mirror_info['prefix']
+
+        if self.in_pos=='nan':
+            self.in_pos = np.nan
+        if self.out_pos=='nan':
+            self.out_pos = np.nan
+
+    def check_coating(self, **kwargs):
+        if kwargs['value']==1:
+            if self.name=='MR1L4':
+                if self.destination=='MFX':
+                    self.current_range = self.mfx_ranges[0]
+                    self.nominal_pitch = self.pitch_rbvs['MFX_Coating1']
+                elif self.destination=='MEC':
+                    self.current_range = self.mec_ranges[0]
+                    self.nominal_pitch = self.pitch_rbvs['MEC_Coating1']
+                else:
+                    self.current_range = None
+            else:
+                self.current_range = self.ranges[0]
+                self.nominal_pitch = self.pitch_rbvs['Coating1']
+        elif kwargs['value']==2:
+            if self.name=='MR1L4':
+                if self.destination=='MFX':
+                    self.current_range = self.mfx_ranges[1]
+                    self.nominal_pitch = self.pitch_rbvs['MFX_Coating2']
+                elif self.destination=='MEC':
+                    self.current_range = self.mec_ranges[1]
+                    self.nominal_pitch = self.pitch_rbvs['MEC_Coating2']
+                else:
+                    self.current_range = None
+            else:
+
+                self.current_range = self.ranges[1]
+                self.nominal_pitch = self.pitch_rbvs['Coating2']
+        else:
+            self.current_range = None
+        self.status.update_nominal(self.nominal_pitch)
+
+    def check_dest(self, **kwargs):
+        if self.name=='MR1L4':
+            if kwargs['value']<=-500:
+                self.destination = 'MFX'
+            elif kwargs['value']>=800:
+                self.destination = 'MEC'
+            else:
+                self.destination = None
+
+    def check_inout(self,**kwargs):
+       
+        if kwargs['value']==2:
+            self.is_in = True
+            self.is_out = False
+        elif kwargs['value']==1:
+            self.is_in = False
+            self.is_out = True
+        else:
+            self.is_in = False
+            self.is_out = False
+        #state = 'Unknown'
+        #if np.abs(kwargs['value'] - self.in_pos)<1000:
+        #    state = 'IN'
+        #    self.is_in = True
+        #elif np.abs(kwargs['value'] - self.out_pos)<1000:
+        #    state = 'OUT'
+        #    self.is_in = False
+        #else:
+        #    self.is_in = False
+  
+    def move_in_thread(self, energy_range, destination=None):
+        thread = threading.Thread(target=self.move_in, args=[energy_range], kwargs={"destination": destination})
+        thread.start()
+
+    def move_pitch(self,**kwargs):
+        self.pitch_motor.set(self.nominal_pitch.get())
+
+    def move_in(self, energy_range, destination=None):
+        if self.is_inout:
+            self.x_state.set(2)
+
+        self.coating_motor.put(energy_range+1,wait=True)
+
+        #time.sleep(0.1)
+        if self.name=='MR1L4':
+            if destination=='MFX':
+                if energy_range==0:
+                    self.nominal_pitch = self.pitch_rbvs['MFX_Coating1']
+                    self.pitch_motor.set(self.nominal_pitch.get())
+                    
+                elif energy_range==1:
+                    self.nominal_pitch = self.pitch_rbvs['MFX_Coating2']
+                    self.pitch_motor.set(self.nominal_pitch.get())
+            elif destination=='MEC':
+                if energy_range==0:
+                    self.nominal_pitch = self.pitch_rbvs['MEC_Coating1']
+                    self.pitch_motor.set(self.nominal_pitch.get())
+                elif energy_range==1:
+                    self.nominal_pitch = self.pitch_rbvs['MEC_Coating2']
+                    self.pitch_motor.set(self.nominal_pitch.get())
+        else:
+            if energy_range==0:
+                self.nominal_pitch = self.pitch_rbvs['Coating1']
+                self.pitch_motor.set(self.nominal_pitch.get())
+            elif energy_range==1:
+                self.nominal_pitch = self.pitch_rbvs['Coating2']
+                self.pitch_motor.set(self.nominal_pitch.get())
+       
+        # wait for mirror to stop moving and then send pitch command again
+
+        self.status.update_nominal(self.nominal_pitch)
+        moving_status = True
+        while moving_status:
+            moving_status = np.logical_and(self.coating_moving.get(),self.pitch_moving.get())
+        time.sleep(1) 
+        if np.abs(self.pitch_rbv.get()-self.nominal_pitch.get())>0.2:
+            self.move_pitch()
+
+    def check_pitch(self):
+        pitch_flag = 1
+        if self.nominal_pitch is not None:
+            if np.abs(self.pitch_rbv.get()-self.nominal_pitch.get())>0.2:
+                pitch_flag=0
+        return pitch_flag
+
+
+    def move_out(self):
+        if self.is_inout: 
+            self.x_state.set(1)
+
---
+++ soms_overview/soms_overview.py
@@ -0,0 +1,231 @@
+import numpy as np
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt, QObject
+from pydm import Display
+import os
+from mirror_module import  HOMS_state
+import subprocess
+
+class App(Display):
+
+    def __init__(self, parent=None, args=None, macros=None):
+        #print('Start of __init__ for template launcher')
+        #print(f'args={args}, macros={macros}')
+        # Call super after handling args/macros but before doing pyqt stuff
+        super().__init__(parent=parent, args=args, macros=macros)
+        # Now it is safe to refer to self.ui and access your widget objects
+        # It is too late to do any macros processing
+        #print('End of __init__ for template launcher')
+        self.ui.hutchComboBox.addItems(['CXI','XCS','MFX','MEC'])
+
+        self.ui.hutchComboBox.currentIndexChanged.connect(self.populate_energy_range)
+        self.populate_energy_range()
+
+        self.ui.moveButton.pressed.connect(self.move_mirrors)
+
+        self.MR1K1 = HOMS_state('MR1K1',status=self.ui.MR1K1_status)
+        self.MR1K3 = HOMS_state('MR1K3',status=self.ui.MR1K3_status)
+        self.MR2K3 = HOMS_state('MR2K3',status=self.ui.MR2K3_status)
+        self.MR1K4 = HOMS_state('MR1K4',status=self.ui.MR1K4_status)
+        self.mirror_list = [self.MR1K1, self.MR1K3, self.MR2K3, self.MR1K4]
+        #self.MR1L0.connect_mirror('MR1L0')
+        #self.MR2L0.connect_mirror('MR2L0')
+        #self.MR1L3.connect_mirror('MR1L3')
+        #self.MR2L3.connect_mirror('MR2L3')
+        #self.MR1L4.connect_mirror('MR1L4')
+        self.prev_destination='Unknown'
+        self.destination = 'XCS'
+        self.curr_range = [0,0]
+        self.bg = 'purple'
+        self.color = 'white'
+
+        self.ui.energyLabel.setStyleSheet("background-color: white")
+        self.ui.beamLabel.setStyleSheet("background-color: white")
+        self.ui.currEnergyLabel.setStyleSheet("background-color: white")           
+        #self.actionChange_Setpoints.triggered.connect(self.change_setpoints)
+        #self.ui.PyDMEmbeddedDisplay.hide()
+        #self.ui.PyDMEmbeddedDisplay_2.hide()
+        #self.ui.PyDMEmbeddedDisplay_3.hide()
+        #self.ui.PyDMEmbeddedDisplay_4.hide()
+        #self.ui.PyDMEmbeddedDisplay_5.hide()
+        #self.show_mr1l0()
+        #self.show_mr2l0()
+        #self.show_mr1l3()
+        #self.show_mr2l3()
+        #self.show_mr1l4()
+
+        #QtCore.QTimer.singleShot(500,self.do_resize)
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(2000)
+        self.timer.timeout.connect(self.check_destination)
+        self.timer.start()
+        QtCore.QTimer.singleShot(500,self.do_resize)
+
+    #def update_mirrors(self):
+    #    self.MR1L0.update_mirror()
+    #    self.MR2L0.update_mirror()
+    #    self.MR1L3.update_mirror()
+    #    self.MR2L3.update_mirror()
+    #    self.MR1L4.update_mirror()
+
+    #def change_setpoints(self):
+    #    setpoint_window = Setpoints(self)
+    #    setpoint_window.saveSig.connect(self.update_mirrors)
+    #    setpoint_window.show()
+
+    #def show_mirrors(self):
+    #    self.dockWidget_2.setVisible(True)
+
+    def do_resize(self):
+        self.resize(self.ui.minimumSizeHint())
+
+    def show_mr1l0(self):
+        pass    
+            
+    def show_mr2l0(self):
+        pass
+    def show_mr1l3(self):
+        pass
+    def show_mr1l4(self):
+        pass
+    def show_mr2l3(self):
+        pass
+    def move_mirrors(self):
+
+        destination = self.ui.hutchComboBox.currentText()
+        energy_range = self.ui.energyComboBox.currentIndex()
+        self.MR1L0.move_in_thread(energy_range)
+        self.MR2L0.move_in_thread(energy_range)
+
+        if destination=='CXI':
+            self.MR1L4.move_out()
+            self.MR1L3.move_out()
+        elif destination=='XCS':
+            self.MR1L3.move_in_thread(energy_range)
+            self.MR2L3.move_in_thread(energy_range)
+            self.MR1L4.move_out()
+        elif destination=='MFX':
+            self.MR1L3.move_out()
+            self.MR1L4.move_in_thread(energy_range,destination='MFX')
+        elif destination=='MEC':
+            self.MR1L3.move_out()
+            self.MR1L4.move_in_thread(energy_range,destination='MEC')
+
+    def check_range(self,*args):
+        minE = 0
+        maxE = 1000
+       
+        for arg in args:
+            if arg is not None:
+                minE = max(minE,arg[0])
+                maxE = min(maxE,arg[1])
+        if maxE<minE:
+            minE = 0
+            maxE = 0
+        return [minE, maxE]
+
+    def check_destination(self):
+        width = 100
+        if self.MR1L3.is_in and self.MR1L4.is_out:
+            self.destination = 'XCS'
+            self.bg = 'purple'
+            self.color = 'white'
+            self.curr_range = self.check_range(self.MR1L0.current_range,self.MR2L0.current_range,self.MR1L3.current_range,self.MR2L3.current_range)
+        elif self.MR1L4.is_in and self.MR1L3.is_out:
+            self.destination = self.MR1L4.destination
+            if self.destination=='MFX':
+                self.bg = 'orange'
+                self.color = 'white'
+            elif self.destination=='MEC':
+                self.bg = 'yellow'
+                self.color = 'black'
+            else:
+                self.bg = 'white'
+                self.color = 'black'
+                self.destination = 'Unknown'
+            self.curr_range = self.check_range(self.MR1L0.current_range,self.MR2L0.current_range,self.MR1L4.current_range)
+        elif self.MR1L4.is_out and self.MR1L3.is_out:
+            self.destination = 'CXI'
+            self.bg = 'red'
+            self.color = 'white'
+            self.curr_range = self.check_range(self.MR1L0.current_range,self.MR2L0.current_range)
+        else:
+            self.destination = 'Unknown'
+            self.bg = 'white'
+            self.color = 'black'
+            self.curr_range = [0,0]
+            width = 200
+
+        #pitch_correct = np.zeros(5)
+
+        #for num, mirror in enumerate(self.mirror_list):
+        #    pitch_correct[num] = mirror.check_pitch()
+
+        #if pitch_correct[0]==1:
+        #    self.ui.pitch_status_label.setText('MR1L0 Pitch Nominal')
+        #    self.ui.pitch_status_label.setStyleSheet('background-color: %s' % ('green'))
+        #else:
+        #    self.ui.pitch_status_label.setText('Check MR1L0 Pitch')
+        #    self.ui.pitch_status_label.setStyleSheet('background-color: %s' % ('red'))
+        
+        
+        self.ui.beamLabel.setText(self.destination)
+        self.ui.beamLabel.setStyleSheet("background-color: %s; color: %s" % (self.bg,self.color))
+        self.ui.beamLabel.setFixedWidth(width)
+        self.ui.energyLabel.setText('%.1f-%.1f keV' % (self.curr_range[0],self.curr_range[1]))
+
+        if self.destination != self.prev_destination:
+            if self.destination=='CXI':
+                self.pitchGroupbox.setTitle('Adjust Undulators')
+                self.NegTweakButton.channel = None# 'ca://MR2L0:HOMS:MMS:PITCH.TWR'
+                self.PosTweakButton.channel = None#'ca://MR2L0:HOMS:MMS:PITCH.TWF'
+                self.TweakAmountLineEdit.channel = None#'ca://MR2L0:HOMS:MMS:PITCH.TWV'
+                self.PitchReadbackLabel.channel = None#'ca://MR2L0:HOMS:MMS:PITCH.RBV'
+
+            elif self.destination=='Unknown':
+                self.pitchGroupbox.setTitle('Wait for transition to complete')
+                self.NegTweakButton.channel = None# 'ca://MR2L0:HOMS:MMS:PITCH.TWR'
+                self.PosTweakButton.channel = None#'ca://MR2L0:HOMS:MMS:PITCH.TWF'
+                self.TweakAmountLineEdit.channel = None#'ca://MR2L0:HOMS:MMS:PITCH.TWV'
+                self.PitchReadbackLabel.channel = None#'ca://MR2L0:HOMS:MMS:PITCH.RBV'
+
+
+
+            elif self.destination=='MFX' or self.destination=='MEC':
+                self.pitchGroupbox.setTitle('MR1L4 Pitch Adjustment')
+                self.NegTweakButton.channel = 'ca://MR1L4:HOMS:MMS:PITCH.TWR'
+                self.PosTweakButton.channel = 'ca://MR1L4:HOMS:MMS:PITCH.TWF'
+                self.TweakAmountLineEdit.channel = 'ca://MR1L4:HOMS:MMS:PITCH.TWV'
+                self.PitchReadbackLabel.channel = 'ca://MR1L4:HOMS:MMS:PITCH.RBV'
+
+            elif self.destination=='XCS':
+                self.pitchGroupbox.setTitle('MR1L3 Pitch Adjustment')
+                self.NegTweakButton.channel = 'ca://MR1L3:HOMS:MMS:PITCH.TWR'
+                self.PosTweakButton.channel = 'ca://MR1L3:HOMS:MMS:PITCH.TWF'
+                self.TweakAmountLineEdit.channel = 'ca://MR1L3:HOMS:MMS:PITCH.TWV'
+                self.PitchReadbackLabel.channel = 'ca://MR1L3:HOMS:MMS:PITCH.RBV'
+
+            self.prev_destination = str(self.destination)
+
+
+
+    def populate_energy_range(self):
+        hutch = self.ui.hutchComboBox.currentText()
+        if hutch=='CXI':
+            energy = ['1-14','12-25']
+        elif hutch=='XCS':
+            energy = ['1-13.5','13-25']
+        elif hutch=='MFX':
+            energy = ['1-13.5','13-25']
+        elif hutch=='MEC':
+            energy = ['1-13.5','13-25']
+        else:
+            energy = ['1-14','12-25']
+        self.ui.energyComboBox.clear()
+        self.ui.energyComboBox.addItems(energy)
+
+
+    def ui_filename(self):
+        return 'soms_overview.ui'
+
